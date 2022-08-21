fa73ae2746148758037457b795a7465f45120320
Vasse <egilmonsas@gmail.com>
2022-08-22 00:04:01 +0200

    started making html pdf report layout

---
+++ pore_pressure/main.py
@@ -0,0 +1,27 @@
+
+import jinja2
+import pandas as pd
+import numpy as np
+from bokeh.embed import components
+from bokeh.plotting import figure
+from bokeh.sampledata.stocks import AAPL
+
+import html_template
+
+#Generate dummy data
+N=10000
+x=np.linspace(0,1000,N)
+y=np.sin(x)+0.2*np.cos(0.2*x)
+
+p = figure(
+    plot_height=250, 
+    sizing_mode="stretch_width",
+    x_axis_type="datetime",
+    output_backend="webgl"
+    )
+p.line(x,y, color='navy', alpha=0.5)
+
+script, div = components(p)
+with open('plot.html', 'w') as f:
+    f.write(html_template.poretrykkdash.render(script=script, div=div)
+            )
