12f6c15ee87f784cb6ebda3c83d6488910314bf9
Vasse <egilmonsas@gmail.com>
2022-08-28 15:21:31 +0200

    added hover support

--- pore_pressure/main.py
+++ pore_pressure/main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 from bokeh.embed import components
 from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, HoverTool
 from piezometer import PiezometerMetaData, PiezometerDataHandler
 
 
@@ -23,16 +23,34 @@ data=data_handler.load_piezometer_data()
 source = ColumnDataSource.from_df(next(data)[1])
 
 
+
+
+
+hover_tool=HoverTool(
+    tooltips=[
+        ( 'Time',   '@Date_Time{%F}'            ),
+        ( 'Absolute pressure',  '@GWL{%0.2f}' ), # use @{ } for field names with spaces
+    ],
+
+    formatters={
+        '@Date_Time': 'datetime',
+        '@GWL'      : 'printf',   
+    },
+
+    line_policy = 'interp'
+)
+
+
 p = figure(
     plot_height=250, 
     sizing_mode="stretch_width",
     x_axis_type="datetime",
     output_backend="webgl",
-    tools=['hover']
+    tools=[hover_tool],
     )
 p.line('Date_Time',"GWL", source=source, color='navy', alpha=0.5)
-
+p.toolbar.logo = None
 script, div = components(p)
 with open('plot.html', 'w') as f:
-    f.write(env.get_template("piezometer_rapport").render(doc_title="gangster",script=script, div=div)
+    f.write(env.get_template("piezometer_rapport").render(doc_title="420kV Songdal Søvassli",script=script, div=div)
         )
