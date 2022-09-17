f35ef921ba630d0cfdbe7cfb0ac67a2b0a61f58a
Vasse <egilmonsas@gmail.com>
2022-09-17 09:03:08 +0200

    started streamlit thing

---
+++ stex/Hjem.py
@@ -0,0 +1,13 @@
+import streamlit as st  
+
+st.set_page_config(
+    page_title="NGI-PVT oppfølging",
+    page_icon ="🏠",
+)
+st.markdown("""
+    
+# Hello
+Is it me you're looking for?
+
+
+""")
---
+++ stex/pages/2_E102.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.set_page_config(page_title="E102")
+st.title("Poretrykksavlesninger")
